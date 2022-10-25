f1dd66742a942e7ac7b9de339f696e5f9c3ca383
dev <im-oswald@github.com>
2022-10-26 03:44:55 +0500

    User registration and Auth middleware added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const auth = require('../../middlewares/auth');
+const User = require('../../models/User');
 
 const router = express.Router();
 
 // @route           /api/auth
 // @descriotion     to do the authentication
 // @access          Private
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    return res.json({ user });
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API is working'));
 
 // Define custom routes here
