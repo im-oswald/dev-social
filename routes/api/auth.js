0d22e5bfbcd19bf57685a33edeb51274eee39566
dev <im-oswald@github.com>
2022-10-27 01:25:05 +0500

    Authentication and profile routes added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const auth = require('../../middlewares/auth');
 const User = require('../../models/User');
+const config = require('config');
 
 const router = express.Router();
 
@@ -15,6 +19,52 @@ router.get('/', auth, async (req, res) => {
   } catch(err) {
     console.log(err);
     return res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
+  }
+});
+
+// @route           /api/auth/login
+// @descriotion     to do the authentication
+// @access          Public
+router.post('/login', [
+  check('email', 'Not a valid email').isEmail(),
+  check('password', 'Password is required').exists()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  } else {
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+      if(!user) {
+        return res.status(401).json({ errors: [{ msg: 'Invalid email or password' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if(isMatch) {
+        const payload = {
+          user: {
+            id: user.id
+          }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 3600 }, (err, token) => {
+          if(err) {
+            throw err;
+          }
+
+          return res.json({ token });
+        })
+      } else {
+        return res.status(401).json({ errors: [{ msg: 'Invalid email or password' }] });
+      }
+
+    } catch(err) {
+      console.log(err);
+      res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
+    }
   }
 });
 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,87 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+const auth = require('./../../middlewares/auth');
+const Profile = require('./../../models/Profile');
 
 const router = express.Router();
 
-// @route           /api/profile
+// @route           /api/profile/me
 // @description     to show the user profile
-// @access          Public
-router.get('/', (req, res) => res.send('Profile route'));
+// @access          Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user_id: req.user.id }).populate('user', ['name', 'avatar']);
+
+    if(!profile) {
+      res.status(400).json({ errors: [{ msg: 'No profile found against the user' }] });
+    }
+
+    res.json(profile);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
+  }
+});
+
+// @route         /api/profile
+// @description   Save and update current user profile
+// @access        Private
+router.post('/', [auth, [
+  check('status').not().isEmpty(),
+  check('skills').not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    company,
+    website,
+    status,
+    location,
+    skills,
+    bio,
+    githubusername,
+    youtube,
+    twitter,
+    facebook,
+    linkedin,
+    instagram
+  } = req.body;
+
+  let profileFields = {};
+  if(company) profileFields.company = company;
+  if(website) profileFields.website = website;
+  if(status) profileFields.status = status;
+  if(location) profileFields.location = location;
+  if(bio) profileFields.bio = bio;
+  if(githubusername) profileFields.githubusername = githubusername;
+  if(skills) profileFields.skills = skills.split(',').map(skill => skill.trim());
+  
+  profileFields.social = {}
+  if(youtube) profileFields.social.youtube = youtube;
+  if(twitter) profileFields.social.twitter = twitter;
+  if(facebook) profileFields.social.facebook = facebook;
+  if(linkedin) profileFields.social.linkedin = linkedin;
+  if(instagram) profileFields.social.instagram = instagram;
+
+  try {
+    let profile = await Profile.findOne({ user_id: req.user.id });
+
+    if(profile) {
+      profile = await Profile.findOneAndUpdate({ user_id: req.user.id }, { $set: profileFields }, { new: true });
+    } else {
+      profile = new Profile(profileFields);
+      profile.save();
+    }
+
+    res.json(profile);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
+  }
+});
 
 module.exports = router;
