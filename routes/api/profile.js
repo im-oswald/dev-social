f29e04f49a33a64ef2979f5cf267caa0fcc85491
dev <im-oswald@github.com>
2022-10-27 02:07:23 +0500

    Delete profile route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -120,6 +120,21 @@ router.get('/user/:user_id', async (req, res) => {
 
     res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
   }
-})
+});
+
+// @route         /api/profile
+// @description   Delete current profile, user and posts
+// @access        Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted successfully' });
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
+  }
+});
 
 module.exports = router;
